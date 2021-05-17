3def0e295be88e94fce79653994233c1a8939aa2
wizkids <wyzekid@gmail.com>
2021-05-17 19:08:17 +0300

    Initial commit

---
+++ src/main/java/com/example/turing/turinginvestments/TuringInvestmentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.turing.turinginvestments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuringInvestmentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuringInvestmentsApplication.class, args);
+    }
+
+}
